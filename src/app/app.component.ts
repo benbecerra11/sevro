1dec4f60a3b2f05197d3d2ab556c2c9e68f54acb
Seth Becerra <seth.becerra11@t-mobile.com>
2023-03-21 18:57:49 -0600

    testing git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title = 'my-first-app ';
+  // Name= "seth"
 }
+//This works-- were essentially creating our own html tag that we then inject into the page dyanmically on build.-sb
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-snoot',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'my-first-snoot-app ';
+// }
